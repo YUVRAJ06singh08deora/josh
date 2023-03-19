224d42832537635c2faeaca01146a8fc850f3fa8
Yuvraj Singh Deora <06yuvraj2001singh@gmail.com>
2023-03-19 23:07:56 +0530

    Home View Of Patient Added

--- app/src/main/java/com/example/josh/doctor_patient_allocated.java
+++ app/src/main/java/com/example/josh/doctor_patient_allocated.java
@@ -2,13 +2,24 @@ package com.example.josh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class doctor_patient_allocated extends AppCompatActivity {
-
+    RelativeLayout patientViewtest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_patient_allocated);
+        patientViewtest=findViewById(R.id.patientsinfo);
+        patientViewtest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),patient_home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
