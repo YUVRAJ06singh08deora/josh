1519ffe228c2929080576b85ab72c50e1467866c
Yuvraj Singh Deora <06yuvraj2001singh@gmail.com>
2023-03-19 22:22:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/josh/doctor_patient_allocated.java
@@ -0,0 +1,14 @@
+package com.example.josh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class doctor_patient_allocated extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_patient_allocated);
+    }
+}
